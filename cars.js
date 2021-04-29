f30021b181c685240beeaf44a9508d3cb6906318
Anurag Reddy Yerrabotula <ayerra4@uic.edu>
2021-04-29 23:32:21 +0000

    added

---
+++ cars.js
@@ -0,0 +1,178 @@
+const cars = [
+
+  {
+    id: "1",
+    name: "Honda Accord LX",
+    price: "20",
+    model: "sedan",
+    type: "regular/comfort",
+    economy: "yes",
+    reliability: "yes"
+  },
+  {
+    id: "2",
+    name: "Honda Accord sport special edition",
+    price: "20",
+    model: "sedan",
+    type: "sport",
+    economy: "yes",
+    reliability: "yes"
+  },
+  {
+    id: "3",
+    name: "Honda Accord Touring",
+    price: "40",
+    model: "sedan",
+    type: "luxury",
+    economy: "yes",
+    reliability: "yes"
+  },
+   {
+    id: "4",
+    name: "Honda CR-V",
+    price: "20",
+    model: "SUV",
+    type: "regular/comfort",
+    economy: "yes",
+    reliability: "yes"
+  },
+   {
+    id: "5",
+    name: "Honda CR-V Hybrid",
+    price: "40",
+    model: "SUV",
+    type: "regular/comfort",
+    economy: "yes",
+    reliability: "yes"
+  },
+   {
+    id: "6",
+    name: "Honda pilot",
+    price: "40",
+    model: "SUV",
+    type: "regular/comfort",
+    economy: "no",
+    reliability: "yes"
+  },
+   {
+    id: "7",
+    name: "Honda civic type-R",
+    price: "40",
+    model: "sedan",
+    type: "sport",
+    economy: "no",
+    reliability: "yes"
+  },
+   {
+    id: "8",
+    name: "Honda Ridgeline",
+    price: "40",
+    model: "pickup",
+    type: "regular/comfort",
+    economy: "no",
+    reliability: "yes"
+  },
+   {
+    id: "9",
+    name: "toyota camry-hybrid",
+    price: "20",
+    model: "sedan",
+    type: "regular/comfort",
+    economy: "yes",
+    reliability: "yes"
+  },
+   {
+    id: "10",
+    name: "toyota gr-supra",
+    price: "40",
+    model: "coupe",
+    type: "sport",
+    economy: "no",
+    reliability: "yes"
+  },
+    {
+    id: "11",
+    name: "toyota gr-supra",
+    price: "60",
+    model: "coupe",
+    type: "sport",
+    economy: "no",
+    reliability: "no"
+  },
+    {
+    id: "12",
+    name: "toyota land cruiser",
+    price: "80",
+    model: "SUV",
+    type: "luxury",
+    economy: "no",
+    reliability: "yes"
+  },
+    {
+    id: "13",
+    name: "toyota sequoia",
+    price: "60",
+    model: "SUV",
+    type: "regular/comfort",
+    economy: "no",
+    reliability: "yes"
+  },
+    {
+    id: "14",
+    name: "ford f-150 xl",
+    price: "20",
+    model: "coupe",
+    type: "pickup",
+    economy: "no",
+    reliability: "no"
+  },
+  {
+    id: "15",
+    name: "ford f-150 Limited",
+    price: "80",
+    model: "sedan",
+    type: "pickup",
+    economy: "no",
+    reliability: "yes"
+  },
+  {
+    id: "16",
+    name: "Ford Mustang EcoBoost",
+    price: "20",
+    model: "coupe",
+    type: "sport",
+    economy: "yes",
+    reliability: "yes"
+  },
+  {
+    id: "17",
+    name: "Dodge Challenger SXT",
+    price: "20",
+    model: "coupe",
+    type: "sport",
+    economy: "no",
+    reliability: "no"
+  },
+  {
+    id: "18",
+    name: "honda Civic coupe",
+    price: "20",
+    model: "coupe",
+    type: "regular/comfort",
+    economy: "yes",
+    reliability: "yes"
+  },
+  {
+    id: "18",
+    name: "2020 Subaru BRZ",
+    price: "20",
+    model: "coupe",
+    type: "regular/comfort",
+    economy: "no",
+    reliability: "no"
+  }
+
+
+];
+
+export default cars;
